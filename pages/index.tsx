e0da735a0410e0e73db08f9c7c95b3f67be8b236
Nermin Vejzovic <104377893+NerminVej@users.noreply.github.com>
2023-06-22 09:59:12 +0200

    Added sign out button + made sure not logged in users can access the main page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,34 @@
 import Image from "next/image";
 import { Inter } from "next/font/google";
+import { NextPageContext } from "next";
+import { getSession, signOut } from "next-auth/react";
+
+export async function getServerSideProps(context: NextPageContext) {
+  const session = await getSession(context);
+
+  if (!session) {
+    return {
+      redirect: {
+        destination: "/auth",
+        permanent: false,
+      },
+    };
+  }
+  return {
+    props: {},
+  };
+}
 
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
-  return <main>
-    <h1> Netflix Clone</h1>
-  </main>;
+  return (
+    <main>
+      <h1 className="text-green-500"> Netflix Clone</h1>
+      <button className="h-10 w-full bg-white" onClick={() => signOut()}>
+        {" "}
+        Sign Out
+      </button>
+    </main>
+  );
 }
