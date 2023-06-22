cb38fc154b484445f0702a78e9795d9b7b1bd47c
Nermin Vejzovic <104377893+NerminVej@users.noreply.github.com>
2023-06-22 10:01:18 +0200

    added commenting on index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,9 +3,12 @@ import { Inter } from "next/font/google";
 import { NextPageContext } from "next";
 import { getSession, signOut } from "next-auth/react";
 
+// This function is executed on the server-side to get the initial props for the page
 export async function getServerSideProps(context: NextPageContext) {
+  // Retrieve the session information using the getSession function from next-auth
   const session = await getSession(context);
 
+  // If there is no session (user is not authenticated), redirect them to the "/auth" page
   if (!session) {
     return {
       redirect: {
@@ -14,17 +17,24 @@ export async function getServerSideProps(context: NextPageContext) {
       },
     };
   }
+
+  // If the user is authenticated, return an empty object as props
   return {
     props: {},
   };
 }
 
+// Load the Inter font subset (in this case, "latin")
 const inter = Inter({ subsets: ["latin"] });
 
+// The default export for the Home component
 export default function Home() {
   return (
     <main>
+      {/* Display a heading with the "text-green-500" CSS class */}
       <h1 className="text-green-500"> Netflix Clone</h1>
+
+      {/* Render a sign out button that triggers the signOut function from next-auth */}
       <button className="h-10 w-full bg-white" onClick={() => signOut()}>
         {" "}
         Sign Out
