6e20f98a6d9e056813a8aeec1d81e537b5166494
Nermin Vejzovic <104377893+NerminVej@users.noreply.github.com>
2023-06-23 12:31:38 +0200

    adding some styling aswell as adding a delete button

--- components/InfoModal.tsx
+++ components/InfoModal.tsx
@@ -28,26 +28,82 @@ const InfoModal: React.FC<InfooModalProps> = ({ visible, onClose }) => {
     }, 300);
   }, [onClose]);
 
-  if(!visible){
+  if (!visible) {
     return null;
   }
 
-  return <div className="
-  z-50
-  transition
-  duration-300
-  bg-black
-  bg-opacity-80
-  flex
-  justify-center
-  items-center
-  overflow-x-hidden
-  overflow-y-auto
-  fixed
-  inset-0
-  ">
+  return (
+    <div
+      className="
+        z-50
+        transition
+        duration-300
+        bg-black
+        bg-opacity-80
+        flex
+        justify-center
+        items-center
+        overflow-x-hidden
+        overflow-y-auto
+        fixed
+        inset-0
+        "
+    >
+      <div
+        className="
+        relative
+        w-auto
+        mx-auto
+        max-w-3xl
+        rounded-md
+        overflow-hidden
+        "
+      >
+        <div
+          className={`
+        ${isVisible ? "scale-100" : "scale-0"}
+        transform
+        duration-300
+        relative
+        flex-auto
+        bg-zinc-900
+        drop-shadow-md
+        `}
+        >
+          <div className="relative h-96">
+            <video
+              className="
+            w-full
+            brightness-[60%]
+            object-cover
+            h-full
+            "
+              autoPlay
+              muted
+              loop
+              poster={data?.thumbnailUrl}
+              src={data?.videoUrl}
+            ></video>
+            <div
+            className="
+            cursor-pointer
+            absolute
+            top-3
+            right-3
+            h-10
+            w-10
+            rounded-full
+            bg-black
+            
+            "
+            onClick={() => {}}>
 
-  </div>;
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default InfoModal;
