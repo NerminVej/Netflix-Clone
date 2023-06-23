c92aec5e2d8b95953542a128eee59be69ab26791
Nermin Vejzovic <104377893+NerminVej@users.noreply.github.com>
2023-06-23 08:41:03 +0200

    added a new componnent called MovieList

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,6 +4,7 @@ import { NextPageContext } from "next";
 import { getSession } from "next-auth/react";
 import Navbar from "@component/components/Navbar";
 import Billboard from "@component/components/Billboard";
+import MovieList from "@component/components/MovieList";
 
 // This function is executed on the server-side to get the initial props for the page
 export async function getServerSideProps(context: NextPageContext) {
@@ -30,11 +31,14 @@ export async function getServerSideProps(context: NextPageContext) {
 const inter = Inter({ subsets: ["latin"] });
 
 // The default export for the Home component
-export default function Home() { 
+export default function Home() {
   return (
     <main>
       <Navbar />
       <Billboard />
+      <div className="pb-40">
+        <MovieList />
+      </div>
     </main>
   );
 }
