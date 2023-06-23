9316a497b655d5d8357f80bb9d114d365d536e4f
Nermin Vejzovic <104377893+NerminVej@users.noreply.github.com>
2023-06-23 12:07:56 +0200

    added a watch folder

---
+++ pages/watch/[movieId].ts
@@ -0,0 +1,11 @@
+import React from "react";
+
+import useMovie from "@component/hooks/useMovie";
+import { useRouter } from "next/router";
+
+const Watch = () => {
+  const router = useRouter();
+  const { movieId } = router.query;
+
+  const { data } = useMovie(movieId as string);
+};
