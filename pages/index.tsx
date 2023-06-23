865c0abafbec3721f01627fcf152d6560d72c8e6
Nermin Vejzovic <104377893+NerminVej@users.noreply.github.com>
2023-06-23 08:51:27 +0200

    fixed an error with the global css styling

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,6 +5,7 @@ import { getSession } from "next-auth/react";
 import Navbar from "@component/components/Navbar";
 import Billboard from "@component/components/Billboard";
 import MovieList from "@component/components/MovieList";
+import useMovieList from "@component/hooks/useMovieList";
 
 // This function is executed on the server-side to get the initial props for the page
 export async function getServerSideProps(context: NextPageContext) {
@@ -32,12 +33,15 @@ const inter = Inter({ subsets: ["latin"] });
 
 // The default export for the Home component
 export default function Home() {
+
+  const { data: movies = [] } = useMovieList();
+
   return (
     <main>
       <Navbar />
       <Billboard />
       <div className="pb-40">
-        <MovieList />
+        <MovieList title="Trending Now" data={movies}/>
       </div>
     </main>
   );
