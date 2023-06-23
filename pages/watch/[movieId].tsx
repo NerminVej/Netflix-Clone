25688c79d3cdcef41f5ef338fefbf1b6578568a1
Nermin Vejzovic <104377893+NerminVej@users.noreply.github.com>
2023-06-23 12:14:17 +0200

    added the video player functionality

--- pages/watch/[movieId].tsx
+++ pages/watch/[movieId].tsx
@@ -2,6 +2,7 @@ import React from "react";
 
 import useMovie from "@component/hooks/useMovie";
 import { useRouter } from "next/router";
+import { AiOutlineArrowLeft } from "react-icons/ai";
 
 const Watch = () => {
   const router = useRouter();
@@ -9,11 +10,40 @@ const Watch = () => {
 
   const { data } = useMovie(movieId as string);
 
-  return(
-    <div>
-
+  return (
+    <div className="h-screen w-screen bg-black">
+      <nav
+        className="
+        fixed
+        w-full
+        p-4
+        z-10
+        flex
+        flex-row
+        items-center
+        gap-8
+        bg-black
+        bg-opacity-70
+        "
+      >
+        <AiOutlineArrowLeft
+          onClick={() => router.push("/")}
+          className="text-white cursor-pointer"
+          size={40}
+        />
+        <p className="text-white text-1xl md:text-3xl font-bold">
+          <span className="font-light">Watching</span>
+          {data?.title}
+        </p>
+      </nav>
+      <video
+        autoPlay
+        controls
+        src={data?.videoUrl}
+        className="h-full w-full"
+      ></video>
     </div>
-  )
+  );
 };
 
 export default Watch;
