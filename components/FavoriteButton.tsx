97c91574dfab3db1eed8866d26f934586b9109c8
Nermin Vejzovic <104377893+NerminVej@users.noreply.github.com>
2023-06-23 10:13:24 +0200

    added some functionality to toggle the favorites

--- components/FavoriteButton.tsx
+++ components/FavoriteButton.tsx
@@ -10,6 +10,33 @@ interface FavoriteButtonProps {
 }
 
 const FavoriteButton: React.FC<FavoriteButtonProps> = ({ movieId }) => {
+  const { mutate: mutateFavorites } = useFavorites();
+  const { data: currentUser, mutate } = useCurrentUser();
+
+  const isFavorite = useMemo(() => {
+    const list = currentUser?.favoriteIds || [];
+
+    return list.includes(movieId);
+  }, [currentUser, movieId]);
+
+  const toggleFavorites = useCallback(async () => {
+    let response;
+
+    if (isFavorite) {
+      response = await axios.delete("/api/favorite", { data: { movieId } });
+    } else {
+      response = await axios.post("/api/favorite", { movieId });
+    }
+    const updatedFavoriteIds = response?.data?.favoriteIds;
+
+    mutate({
+      ...currentUser,
+      favoriteIds: updatedFavoriteIds,
+    });
+
+    mutateFavorites();
+  }, [movieId, isFavorite, currentUser, mutate, mutateFavorites]);
+
   return (
     <div
       className="
