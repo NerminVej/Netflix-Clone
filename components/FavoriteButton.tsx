fc466f17acaf98921c7724f2a9f3e8a1789fb3d1
Nermin Vejzovic <104377893+NerminVej@users.noreply.github.com>
2023-06-23 10:03:52 +0200

    added FavoriteButton component

---
+++ components/FavoriteButton.tsx
@@ -0,0 +1,21 @@
+import React, { useCallback, useMemo } from 'react'
+import axios from 'axios';
+
+import useCurrentUser from "@component/hooks/useCurrentUser";
+import useFavorites from "@component/hooks/useFavorites";
+
+interface FavoriteButtonProps {
+    movieId: string;
+}
+
+const FavoriteButton: React.FC<FavoriteButtonProps> = ({
+    movieId
+}) => {
+  return (
+    <div >
+
+    </div>
+  )
+}
+
+export default FavoriteButton
