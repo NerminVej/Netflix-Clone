212227db6c56f26b7edfa6c9b5525f4c39ac62a7
Nermin Vejzovic <104377893+NerminVej@users.noreply.github.com>
2023-06-23 10:36:48 +0200

    added some styling to the play button

--- components/PlayButton.tsx
+++ components/PlayButton.tsx
@@ -1,21 +1,33 @@
-import React from 'react'
+import React from "react";
 import { BsFillPlayFill } from "react-icons/bs";
-import { useRouter } from 'next/router';
-
+import { useRouter } from "next/router";
 
 interface PlayButtonProps {
-    movieId: string;
+  movieId: string;
 }
 
-const PlayButton: React.FC<PlayButtonProps> = ({
-    movieId
-}) => {
-    const router = useRouter();
+const PlayButton: React.FC<PlayButtonProps> = ({ movieId }) => {
+  const router = useRouter();
   return (
-    <div>
-
+    <div
+      className="
+    bg-white
+    rounded-md
+    py1 md:py-2
+    px-2 md:px-4
+    w-auto
+    text-xs lg:text-lg
+    font-semibold
+    flex
+    flex-row
+    items-center
+    hover:bg-neutral-300
+    transition
+    "
+    >
+      Play
     </div>
-  )
-}
+  );
+};
 
-export default PlayButton
+export default PlayButton;
