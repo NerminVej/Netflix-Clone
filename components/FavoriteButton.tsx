42adbf4a96af9a5315db2b8b807d60d1b8dd8e6e
Nermin Vejzovic <104377893+NerminVej@users.noreply.github.com>
2023-06-23 10:07:11 +0200

    added a favorite button with a "+" sign on it

--- components/FavoriteButton.tsx
+++ components/FavoriteButton.tsx
@@ -1,8 +1,11 @@
 import React, { useCallback, useMemo } from 'react'
+import { AiOutlinePlus } from 'react-icons/ai';
 import axios from 'axios';
 
 import useCurrentUser from "@component/hooks/useCurrentUser";
 import useFavorites from "@component/hooks/useFavorites";
+
+
 
 interface FavoriteButtonProps {
     movieId: string;
@@ -12,8 +15,23 @@ const FavoriteButton: React.FC<FavoriteButtonProps> = ({
     movieId
 }) => {
   return (
-    <div className=''>
-
+    <div className='
+    cursor-pointer
+    group/item
+    w-6
+    h-6
+    lg:w-10
+    lg:h-10
+    border-white
+    border-2
+    rounded-full
+    flex
+    justify-center
+    items-center
+    transition
+    hover:border-neutral-300
+    '>
+<AiOutlinePlus className='text-white' size={25}/>
     </div>
   )
 }
